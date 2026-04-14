85dea959ba1652cbc3962953b9b1eac7bfab7227
aadrips <adriopos17@gmail.com>
2026-04-14 13:31:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,17 +4,21 @@ app = Flask(__name__)
 
 action_queue = []
 
-@app.route('/action/<name>')
-def action(name):
-    action_queue.append(name)
-    print("Acción:", name)
+@app.route('/action/<action>')
+def add_action(action):
+    action_queue.append(action)
+    print("ACCION RECIBIDA:", action)
     return "OK"
 
 @app.route('/getAction')
 def get_action():
-    if action_queue:
-        return jsonify({"action": action_queue.pop(0)})
+    if len(action_queue) > 0:
+        action = action_queue.pop(0)
+        print("ENVIANDO:", action)
+        return jsonify({"action": action})
+
     return jsonify({"action": None})
 
 if __name__ == "__main__":
+    print("SERVER INICIADO")
     app.run(host="0.0.0.0", port=10000)
