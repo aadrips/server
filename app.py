8ea75b36d81fb1499b78f1a6a2d7f44b361ce4de
aadrips <adriopos17@gmail.com>
2026-04-14 13:41:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,33 @@
+from flask import Flask, jsonify, request
 import os
-from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 action_queue = []
 
+@app.route('/')
+def home():
+    print("PING HOME")
+    return "OK SERVER RUNNING"
+
 @app.route('/action/<action>')
 def add_action(action):
+    print("🔥 ACTION RECIBIDA:", action)
     action_queue.append(action)
-    print("ACCION:", action)
-    return "OK"
+    return {"status": "ok", "action": action}
 
 @app.route('/getAction')
 def get_action():
+    print("📡 ROBLOX PIDE ACTION")
+
     if action_queue:
-        return jsonify({"action": action_queue.pop(0)})
+        action = action_queue.pop(0)
+        print("➡ ENVIANDO:", action)
+        return jsonify({"action": action})
+
     return jsonify({"action": None})
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
-    print("SERVER ON PORT", port)
+    print("🚀 SERVER STARTED ON PORT", port)
     app.run(host="0.0.0.0", port=port)
