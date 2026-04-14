76a44238062d5021261fcdb4cf7abafdce5057f0
aadrips <adriopos17@gmail.com>
2026-04-14 13:37:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify
 
 app = Flask(__name__)
@@ -7,18 +8,16 @@ action_queue = []
 @app.route('/action/<action>')
 def add_action(action):
     action_queue.append(action)
-    print("ACCION RECIBIDA:", action)
+    print("ACCION:", action)
     return "OK"
 
 @app.route('/getAction')
 def get_action():
-    if len(action_queue) > 0:
-        action = action_queue.pop(0)
-        print("ENVIANDO:", action)
-        return jsonify({"action": action})
-
+    if action_queue:
+        return jsonify({"action": action_queue.pop(0)})
     return jsonify({"action": None})
 
 if __name__ == "__main__":
-    print("SERVER INICIADO")
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    print("SERVER ON PORT", port)
+    app.run(host="0.0.0.0", port=port)
